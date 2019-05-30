f142e15f13e3238951ffc124ce7d3305059118b0
AN K <a2nkale@gmail.com>
2019-05-30 19:50:59 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,4 +1,5 @@
 def instantiate_new_array
+<<<<<<< HEAD
   array = Array.new()
   return array 
 end 
@@ -30,4 +31,27 @@ end
 
 def length_of_array (array)
   return array.length 
+=======
+array = Array.new()
+return array 
+end 
+
+def array_with_two_elements
+array = Array.new(2)
+return array
+end 
+def first_element(array)
+  return array.first()
+end 
+def third_element(array)
+end
+def last_element(array)
+end 
+def first_element_with_array_methods
+end 
+def last_element_with_array_methods
+end 
+
+def 
+>>>>>>> fa1c95925bf75082a348907dc86cf0da682f51c6
 end 
